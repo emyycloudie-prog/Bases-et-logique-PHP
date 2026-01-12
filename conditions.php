eba5ea234664c7d5b92fc6b1c336a1f18e5e9e61
emyycloudie-prog <emyycloudie@gmail.com>
2026-01-12 15:59:40 +0100

    Add files via upload

---
+++ conditions.php
@@ -0,0 +1,42 @@
+<?php
+
+// -------- Étape 1 : if / else --------
+
+$age = 17;
+
+if ($age >= 18) {
+    echo "Vous êtes majeur.<br>";
+} else {
+    echo "Vous êtes mineur.<br>";
+}
+
+// --- Étape 2 : if / elseif / else --
+$note = 14;
+
+if ($note >= 16) {
+    echo "Très bien<br>";
+} elseif ($note >= 10) {
+    echo "Passable<br>";
+} else {
+    echo "Échec<br>";
+}
+
+// --- Étape 3 : switch ----
+
+$jour = "Vendredi";
+
+switch ($jour) {
+    case "Lundi":
+        echo "Début de semaine<br>";
+        break;
+
+    case "Vendredi":
+        echo "Dernier jour avant le week-end<br>";
+        break;
+
+    default:
+        echo "Jour normal<br>";
+        break;
+}
+
+?>
